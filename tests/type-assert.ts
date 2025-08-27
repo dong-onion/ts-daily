6eb7a7deb59f691a33752427bebe4580acdbc99b
dong-onion <dong.onionion@gmail.com>
2025-08-27 13:46:24 +0900

    feat: Refactor Equal type implementation and reorganize type assertions

--- tests/type-assert.ts
+++ tests/type-assert.ts
@@ -1,7 +1,19 @@
-export type Equal<A, B> = (<T>() => T extends A ? 1 : 2) extends <
+export type Expect<T extends true> = T;
+
+// 표준 Equal 구현
+export type Equal<X, Y> = (<T>() => T extends X ? 1 : 2) extends <
   T
->() => T extends B ? 1 : 2
+>() => T extends Y ? 1 : 2
   ? true
   : false;
 
-export type Expect<T extends true> = T;
+// 교차 타입(&)이나 매핑 결과를 한 번 펼쳐 "표면 형태"를 정리
+export type Prettify<T> = { [K in keyof T]: T[K] } & {};
+
+// (선택) 표면 형태에 덜 민감한 동치 비교
+export type Extends<A, B> = [A] extends [B] ? true : false;
+export type Equivalent<A, B> = Extends<A, B> extends true
+  ? Extends<B, A> extends true
+    ? true
+    : false
+  : false;
